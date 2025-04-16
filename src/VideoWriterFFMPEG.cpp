a45b6b71a6df75c9986dd884cade6b1de8c27dc1
josura <giorgiolocicero@hotmail.com>
2025-04-16 16:11:12 +0000

    chore: changed logging when throwing exceptions

--- src/VideoWriterFFMPEG.cpp
+++ src/VideoWriterFFMPEG.cpp
@@ -13,22 +13,22 @@ VideoWriterFFMPEG::VideoWriterFFMPEG(const std::string& filename, int width, int
 
     avformat_alloc_output_context2(&format_ctx_, nullptr, nullptr, filename.c_str());
     if (!format_ctx_) {
-        throw std::runtime_error("Could not allocate output format context");
+        throw std::runtime_error("[THROW] VideoWriterFFMPEG::VideoWriterFFMPEG: Could not allocate output format context");
     }
 
     codec_ = avcodec_find_encoder(AV_CODEC_ID_H264);
     if (!codec_) {
-        throw std::runtime_error("H.264 encoder not found");
+        throw std::runtime_error("[THROW] VideoWriterFFMPEG::VideoWriterFFMPEG: H.264 encoder not found");
     }
 
     video_stream_ = avformat_new_stream(format_ctx_, nullptr);
     if (!video_stream_) {
-        throw std::runtime_error("Could not create new stream");
+        throw std::runtime_error("[THROW] VideoWriterFFMPEG::VideoWriterFFMPEG: Could not create new stream");
     }
 
     codec_ctx_ = avcodec_alloc_context3(codec_);
     if (!codec_ctx_) {
-        throw std::runtime_error("Could not allocate codec context");
+        throw std::runtime_error("[THROW] VideoWriterFFMPEG::VideoWriterFFMPEG: Could not allocate codec context");
     }
 
     codec_ctx_->codec_id = AV_CODEC_ID_H264;
@@ -46,40 +46,40 @@ VideoWriterFFMPEG::VideoWriterFFMPEG(const std::string& filename, int width, int
     }
 
     if (avcodec_open2(codec_ctx_, codec_, nullptr) < 0) {
-        throw std::runtime_error("Could not open codec");
+        throw std::runtime_error("[THROW] VideoWriterFFMPEG::VideoWriterFFMPEG: Could not open codec");
     }
 
     if (avcodec_parameters_from_context(video_stream_->codecpar, codec_ctx_) < 0) {
-        throw std::runtime_error("Could not copy codec parameters");
+        throw std::runtime_error("[THROW] VideoWriterFFMPEG::VideoWriterFFMPEG: Could not copy codec parameters");
     }
 
     video_stream_->time_base = codec_ctx_->time_base;
 
     if (!(format_ctx_->oformat->flags & AVFMT_NOFILE)) {
         if (avio_open(&format_ctx_->pb, filename.c_str(), AVIO_FLAG_WRITE) < 0) {
-            throw std::runtime_error("Could not open output file");
+            throw std::runtime_error("[THROW] VideoWriterFFMPEG::VideoWriterFFMPEG: Could not open output file");
         }
     }
 
     if (avformat_write_header(format_ctx_, nullptr) < 0) {
-        throw std::runtime_error("Error occurred when writing header");
+        throw std::runtime_error("[THROW] VideoWriterFFMPEG::VideoWriterFFMPEG: Error occurred when writing header");
     }
 
     frame_ = av_frame_alloc();
     if (!frame_) {
-        throw std::runtime_error("Could not allocate frame");
+        throw std::runtime_error("[THROW] VideoWriterFFMPEG::VideoWriterFFMPEG: Could not allocate frame");
     }
     frame_->format = codec_ctx_->pix_fmt;
     frame_->width = codec_ctx_->width;
     frame_->height = codec_ctx_->height;
 
     if (av_frame_get_buffer(frame_, 32) < 0) {
-        throw std::runtime_error("Could not allocate frame data");
+        throw std::runtime_error("[THROW] VideoWriterFFMPEG::VideoWriterFFMPEG: Could not allocate frame data");
     }
 
     pkt_ = av_packet_alloc();
     if (!pkt_) {
-        throw std::runtime_error("Could not allocate packet");
+        throw std::runtime_error("[THROW] VideoWriterFFMPEG::VideoWriterFFMPEG: Could not allocate packet");
     }
 
     sws_ctx_ = sws_getContext(
@@ -88,7 +88,7 @@ VideoWriterFFMPEG::VideoWriterFFMPEG(const std::string& filename, int width, int
         SWS_BILINEAR, nullptr, nullptr, nullptr
     );
     if (!sws_ctx_) {
-        throw std::runtime_error("Could not initialize sws context");
+        throw std::runtime_error("[THROW] VideoWriterFFMPEG::VideoWriterFFMPEG: Could not initialize sws context");
     }
 }
 
@@ -125,7 +125,7 @@ VideoWriterFFMPEG::~VideoWriterFFMPEG() {
 
 void VideoWriterFFMPEG::write_frame(const uint8_t* rgba_data) {
     if (av_frame_make_writable(frame_) < 0) {
-        throw std::runtime_error("Frame not writable");
+        throw std::runtime_error("[THROW] VideoWriterFFMPEG::write_frame: Frame not writable");
     }
 
     const uint8_t* in_data[1] = { rgba_data };
@@ -136,7 +136,7 @@ void VideoWriterFFMPEG::write_frame(const uint8_t* rgba_data) {
     frame_->pts = frame_index_++;
 
     if (avcodec_send_frame(codec_ctx_, frame_) < 0) {
-        throw std::runtime_error("Error sending frame to encoder");
+        throw std::runtime_error("[THROW] VideoWriterFFMPEG::write_frame: Error sending frame to encoder");
     }
 
     while (avcodec_receive_packet(codec_ctx_, pkt_) == 0) {
@@ -144,7 +144,7 @@ void VideoWriterFFMPEG::write_frame(const uint8_t* rgba_data) {
         pkt_->stream_index = video_stream_->index;
 
         if (av_interleaved_write_frame(format_ctx_, pkt_) < 0) {
-            throw std::runtime_error("Error writing packet");
+            throw std::runtime_error("[THROW] VideoWriterFFMPEG::write_frame: Error writing packet");
         }
         av_packet_unref(pkt_);
     }
